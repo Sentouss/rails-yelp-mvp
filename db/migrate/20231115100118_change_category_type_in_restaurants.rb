20e9af1c39de6b328a513c19167aeb2e1287d728
Ismael Sentissi <ismael.sentissi@gmail.com>
2023-11-15 12:42:21 +0100

    OK refacto

---
+++ db/migrate/20231115100118_change_category_type_in_restaurants.rb
@@ -0,0 +1,5 @@
+class ChangeCategoryTypeInRestaurants < ActiveRecord::Migration[7.1]
+  def change
+    change_column :restaurants, :category, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+puts "Cleaning Restaurant database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+soleil_anatolie = {name: "Soleil D'Anatolie", address: "1 Rue Etienne Dolet, 75020 Paris", category: "belgian"}
+auberge_artistes =  {name: "Auberge des Artistes", address: "132 Bd de Ménilmontant, 75020 Paris", phone_number: "01 43 61 11 65", category: "french"}
+a_deux_pas = {name: "A Deux Pas", address: "55 Rue Saint-Maur, 75011 Paris", phone_number: "01 43 55 67 52", category: "french"}
+paloma = {name: "Paloma", address: "93 Rue Julien Lacroix, 75020 Paris", phone_number: "06 65 96 88 46", category: "italian"}
+le_plaisir = {name: "Le Plaisir", address: "14 Rue de Belleville, 75020 Paris", phone_number: "01 40 33 13 13", category: "french"}
+
+[soleil_anatolie, auberge_artistes, a_deux_pas, paloma, le_plaisir].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
